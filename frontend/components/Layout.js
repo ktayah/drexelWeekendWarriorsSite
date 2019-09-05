18b86d3a79dfba6212010eb0c01dc5fe1c8df750
Adam Bengis <itbengis@gmail.com>
2019-09-04 21:27:34 -0700

    css hoverboxes on icons, added custom stylesheet

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -10,6 +10,7 @@ const Layout = ({children, title}) => (
         <Head>
             <title>{title}</title>
             <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css"/>
+            <link rel="stylesheet" href="static/styles/wwstyles.css"/>
             <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png" />
             <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png" />
--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -45,15 +45,17 @@ class Navbar extends Component {
                             </li>
                         </ul>
                         <div className="form-inline py-1 my-lg-0 mr-4">
-                            {/* TODO: add selection boxes */}
                             <a href="https://www.facebook.com/DrexelWeekendWarriors">
-                                <ion-icon name="logo-facebook" size="large" />
+                                <ion-icon name="logo-facebook" size="large" class="img-hover-border rounded" />
                             </a>
                             <a href="https://www.instagram.com/DrexelWeekendWarriors" className="mx-2">
-                                <ion-icon name="logo-instagram" size="large" />
+                                <ion-icon name="logo-instagram" size="large" class="img-hover-border rounded" />
                             </a>
                             <a href="https://dragonlink.drexel.edu/organization/drexelweekendwarriors">
-                                <img src="https://dragonlink.drexel.edu/favicon.ico" className="pl-1 pb-1" style={{maxWidth: 28}} />
+                                <img 
+                                    src="https://dragonlink.drexel.edu/favicon.ico" 
+                                    className="img-hover-border rounded p-1 mb-1" 
+                                    style={{maxWidth: 33}} />
                             </a>
                         </div>
                         <form className="form-inline my-1 my-lg-0">
