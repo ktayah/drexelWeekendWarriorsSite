8039e16f4164985d41d7cbc3f1c1fab0ed8960d3
Kevin Tayah <ktayah@yahoo.com>
2019-09-13 21:25:40 -0400

    The workings of tripCard

--- frontend/components/TripCard.js
+++ frontend/components/TripCard.js
@@ -3,13 +3,16 @@ import config from '../config';
 const apiUrl = config.development ? config.apiDevelopment : config.api;
 
 const TripCard = (props) => (
-    <div className="trip">
-        <img className="-img-top" src={`${apiUrl + props.props.tripPhoto.url}`} alt="trip image cap" />
-        <div className="trip-body">
-            <h5 className="trip-title">{props.props.tripName}</h5>
-            <p className="trip-text">{props.props.tripDescription}</p>
-            <p className="trip-text"><small className="text-muted">{props.props.create_at}</small></p>
+    <div className="tripCard">
+        <div className="card">
+            <img className="card-img-top" src={`${apiUrl + props.props.tripPhoto.url}`} alt="trip image cap" />
+            <div className="card-body">
+                <h5 className="card-title">{props.props.tripName}</h5>
+                <p className="card-text">{props.props.tripDescription}</p>
+                <p className="card-text"><small className="text-muted">{props.props.create_at}</small></p>
+            </div>
         </div>
+
     </div>
 );
 
--- frontend/components/TripList.js
+++ frontend/components/TripList.js
@@ -1,5 +1,11 @@
 import TripCard from './TripCard';
 
+const TripTable = ({children}) => (
+    <div className="row">
+        {children}
+    </div>
+);
+
 const NoTripsScreen = () => (
     <div>
         <h1>No Trips available, check at another time.</h1>
@@ -7,11 +13,11 @@ const NoTripsScreen = () => (
 );
 
 const TripList = ({trips}) => (
-    <div className="trip-group">
+    <div className="trips">
         {trips.length > 0 ? (
             trips.map(trip => {
                 console.log('trip', trip);
-                return <TripCard props={trip} />
+                return <TripCard key={trip.id} props={trip} />
         })) : (
             <NoTripsScreen />
         )}
@@ -22,10 +28,6 @@ TripList.getInitialProps = (props) => {
     return {
         ...props
     }
-    // return { trips: [{
-    //     title: 'Test',
-    //     desc: 'Sit occaecat incididunt duis duis tempor quis nulla tempor Lorem velit culpa pariatur sint. Adipisicing aliqua sit laboris irure sint aliqua in ad id quis. Occaecat nulla irure duis aute tempor sint adipisicing minim cillum irure ex esse laborum dolor. Elit anim duis proident velit eiusmod sunt Lorem et. Voluptate velit adipisicing sint quis laborum nostrud fugiat Lorem ad incididunt sit incididunt minim voluptate. Commodo nisi sunt dolor cupidatat exercitation irure ad quis elit deserunt non excepteur nostrud consectetur.em'
-    // }]};
 }
 
 export default TripList;
