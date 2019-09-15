da967321a9f1eb0fb2318a1d2c95ac1c80df4694
Kevin Tayah <ktayah@yahoo.com>
2019-09-15 17:13:11 -0400

    Finished styling and functionality of contact us page, still need to find out what is going on backend to complete the process

--- frontend/pages/contact.js
+++ frontend/pages/contact.js
@@ -1,49 +1,84 @@
 import Layout from "../components/Layout";
+import axios from 'axios';
+import config from '../config';
 
-const sendEmail = (data) => {
-    
+class ContactForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: null,
+            lastName : null,
+            email: null,
+            subject: null,
+            message: null
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.sendEmail = this.sendEmail.bind(this);
+    }
+    handleChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+    }
+    sendEmail = async () => {
+        const apiUrl = config.development ? config.apiDevelopment : config.api;
+        return await axios.post(`${apiUrl}/email`, {
+            // firstName: this.state.firstName,
+            // lastName: this.state.lastName,
+            to: 'ktayah@yahoo.com',
+            from: this.state.email,
+            replyTo: 'ktayah@yahoo.com',
+            subject: this.state.subject,
+            text: this.state.message,
+            html: this.state.message
+        });
+    }
+
+    render() {
+        return (
+            <form>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label htmlFor="inputFirstName">First Name</label>
+                        <input type="text" name="firstName" onChange={this.handleChange} className="form-control" id="inputFirstName" placeholder="First Name" />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="inputLastName">Last Name</label>
+                        <input type="text" name="lastName" onChange={this.handleChange} className="form-control" id="inputLastName" placeholder="Last Name" />
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-12">
+                        <label htmlFor="inputEmail">Email</label>
+                        <input type="email" name="email" onChange={this.handleChange} className="form-control" id="inputEmail" placeholder="Email" />
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-12">
+                        <label htmlFor="inputSubject">Subject</label>
+                        <input type="text" name="subject" onChange={this.handleChange} className="form-control" id="inputSubject" placeholder="Subject" />
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-12">
+                        <label htmlFor="inputMessage">Message</label>
+                        <textarea name="message" onChange={this.handleChange} className="form-control" id="inputMessage" placeholder="Enter your Message" rows="5" />
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-12">
+                    <button className="btn btn-primary" type="Submit" onClick={this.sendEmail()}>Send an Email</button>
+                    </div>
+                </div>
+            </form>
+        );
+    }
 }
 
-const ContactForm = () => (
-    <form>
-        <div className="form-row">
-            <div className="form-group col-md-6">
-                <label for="inputFirstName">First Name</label>
-                <input type="text" className="form-control" id="inputFirstName" placeholder="First Name" />
-            </div>
-            <div className="form-group col-md-6">
-                <label for="inputLastName">Last Name</label>
-                <input type="text" className="form-control" id="inputLastName" placeholder="Last Name" />
-            </div>
-        </div>
-        <div className="form-row">
-            <div className="form-group col-md-12">
-                <label for="inputEmail">Email</label>
-                <input type="email" className="form-control" id="inputEmail" placeholder="Email" />
-            </div>
-        </div>
-        <div className="form-row">
-            <div className="form-group col-md-12">
-                <label for="inputSubject">Subject</label>
-                <input type="text" className="form-control" id="inputSubject" placeholder="Subject" />
-            </div>
-        </div>
-        <div className="form-row">
-            <div className="form-group col-md-12">
-                <label for="inputMessage">Message</label>
-                <textarea className="form-control" id="inputMessage" placeholder="Enter your Message" rows="5" />
-            </div>
-        </div>
-        <div className="form-row">
-            <div className="form-group col-md-12">
-               <button class="btn btn-primary" type="Submit" onclick={sendEmail()}>Send an Email</button>
-            </div>
-        </div>
-    </form>
-);
-
-const Contact = ({title}) => (
-    <Layout title={title} activePage='contact'>
+const Contact = ({title}) => (<Layout title={title} activePage='contact'>
         <div className="container">
             <br />
             <h1 className='display-5 text-center'>Send us an email</h1>
@@ -55,7 +90,7 @@ const Contact = ({title}) => (
 
 Contact.getInitialProps = () => {
     return {
-        title: 'Weekend Warriors'
+        title: 'Weekend Warriors',
     }
 }
 
