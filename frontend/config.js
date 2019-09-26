278b80c2b957099af2dab393459930956078053e
Kevin Tayah <ktayah@yahoo.com>
2019-09-26 01:55:13 -0400

    export error with config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,9 +1,7 @@
-module.exports = {
-    config: {
-        "logActions": true,
-        "apiDevelopment": "http://localhost:1337",
-        "api": "http://api.drexelww.com",
-        "sendGridKey": process.env.SENDGRID_API_KEY,
-        "development": false
-    }
+export default config = {
+    "logActions": true,
+    "apiDevelopment": "http://localhost:1337",
+    "api": "http://api.drexelww.com",
+    "sendGridKey": process.env.SENDGRID_API_KEY,
+    "development": false
 }
