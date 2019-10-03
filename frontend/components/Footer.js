7fef3ffe0e5a10b2d1efdc6772e75d418dfeca2b
Kevin Tayah <kevin.tayah@nuix.com>
2019-10-03 01:44:21 -0400

    Changed styling on footer.js to fix safari bugs

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,5 +1,5 @@
 const Footer = () => (
-    <footer className="mt-auto mx-0 py-3 px-3 bg-primary row justify-content-between">
+    <footer className="p-3 mt-auto text-center bg-primary">
         {/* TODO: add tiny ww logo */}
         <span className="col-auto pr-0 pl-4 my-auto">
             <img src="static/images/logo-tiny.png" alt="logo-tiny" className="img-fluid" style={{maxHeight: "3em"}}/>
