6827b30f51749fa910e699f3e52336879161f7a3
Kevin Tayah <kevin.tayah@nuix.com>
2019-10-13 21:40:17 -0400

    Added SEO meta data for link appearance

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -18,6 +18,11 @@ const Layout = ({children, title, activePage}) => (
             <link rel="mask-icon" href="/static/favicon/safari-pinned-tab.svg" color="#5bbad5" />
             <meta name="msapplication-TileColor" content="#00aba9" />
             <meta name="theme-color" content="#ffffff" />
+            <meta property="og:image" content="/static/favicon/favicon-32x32.png" />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="drexelww.com" />
+            <meta property="og:title" content="Drexel Weekend Warriors" />
+            <meta property="og:description" content="Weekend Warriors offers adventures for our fellow students at Drexel, so what are you waiting for. Join us along for the adventures" />
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
