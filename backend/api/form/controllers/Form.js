45aa6ff5819ebc88016f8085636b02ee1aaba3f2
Kevin Tayah <ktayah@yahoo.com>
2020-06-08 14:38:50 -0400

    Added 2 new custom endpoints for form token generation and validation, also added new model components for form and other form related stuff

--- backend/api/form/controllers/Form.js
+++ backend/api/form/controllers/Form.js
@@ -1,3 +1,5 @@
+const { sanitizeEntity } = require('strapi-utils');
+const Cryptr = require('cryptr');
 'use strict';
 
 /**
@@ -5,4 +7,77 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+	// POST /getFormToken
+	getFormToken: async ctx => {
+		try {
+			const { isMultiSignUp, tripId } = ctx.request.body;
+			const signUpType = isMultiSignUp ? 'multi' : 'single';
+			const cryptr = new Cryptr(process.env.FORM_GENERATE_HASH_TOKEN);
+			const hashCode = cryptr.encrypt(JSON.stringify({
+				timeCreated: Date.now(),
+				tripId,
+				signUpType
+			}));
+      
+			await ctx.send({
+				status: 200,
+				formToken: hashCode
+			});
+		} catch (err) {
+			console.error(err);
+			await ctx.send({
+				status: 404,
+				err: 'Contact IT Officer'
+			});
+		}
+	},
+	// POST /checkFormToken
+	checkFormToken: async ctx => {
+		try {
+			const { hashCode } = ctx.request.body;
+			const cryptr = new Cryptr(process.env.FORM_GENERATE_HASH_TOKEN);
+			const formQuery = await strapi.query('form').findOne({hashCode});
+			const { signUpType } = JSON.parse(cryptr.decrypt(hashCode));
+			console.log(formQuery, signUpType);
+
+			const isValidForm = (signUpType === 'single' && formQuery === null) || signUpType === 'multi';
+			await ctx.send({
+				status: 200,
+				isValid: isValidForm
+			});
+		} catch(err) {
+			console.error(err);
+			await ctx.send({
+				status: 404,
+				err: 'Contact IT Officer'
+			});
+		}
+	},
+	// POST /forms
+	create: async ctx => {
+		try {
+			const { hashCode } = ctx.request.body;
+			const cryptr = new Cryptr(process.env.FORM_GENERATE_HASH_TOKEN);
+			console.log(hashCode, cryptr.decrypt(hashCode));
+			const { signUpType } = JSON.parse(cryptr.decrypt(hashCode));
+      
+			// eslint-disable-next-line no-undef
+			if (signUpType === 'single' && await strapi.query('form').findOne({hashCode})) {
+				return await ctx.send({
+					status: 401,
+					err: 'Unauthorized request, did not submit form'
+				});
+			}
+
+			const entity = await strapi.services.form.create(ctx.request.body);
+			return sanitizeEntity(entity, { model: strapi.models.form });
+		} catch (err) {
+			console.log(err);
+			await ctx.send({
+				status: 404,
+				err: err
+			});
+		}
+	},
+};
