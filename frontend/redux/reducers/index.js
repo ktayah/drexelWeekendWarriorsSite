1af83c6a22d847932689df44c36bc09f46d13bf8
Kevin Tayah <ktayah@yahoo.com>
2020-06-14 15:48:36 -0400

    Fixed accidental removal of types folder and removed tripForm reducer in index reducer

--- frontend/redux/reducers/index.js
+++ frontend/redux/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import authenticate from './authenticate';
-import tripForm from './tripForm';
 
 const rootReducer = combineReducers({
     authenticate,
-    tripForm
 });
 
 export default rootReducer;
