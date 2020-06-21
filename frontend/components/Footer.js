0d202a323f3064f538704d48d0f421e6061f7126
Kevin Tayah <ktayah@yahoo.com>
2020-06-20 23:56:39 -0400

    Got rid of unneccassary comment

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,6 +1,5 @@
 const Footer = () => (
     <footer id="footer" className="p-3 mt-auto text-center bg-primary">
-        {/* TODO: add tiny ww logo */}
         <span className="col-auto pr-0 pl-4 my-auto">
             <img src="/images/logo-tiny.png" alt="logo-tiny" className="img-fluid" style={{maxHeight: "3em"}}/>
         </span>
